4694a2e1d75d37a7f68fb5892f2f3f8aec2d07e9
lincolnsmithy <lincolnsmithy@gmail.com>
2024-09-12 19:21:12 -0400

    First commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from itsmithflask!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ itsmithy.py
@@ -0,0 +1,15 @@
+import datetime
+from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("base.html", title="Jinja and Flask", utc_dt=datetime.datetime.utcnow())
+
+@app.route("/list")
+def hello_world():
+    #return "<p>Hello, hmmmItSmiKDKDKDthy</p>"
+    items = ['Item 1', 'Item 2', 'Item 3']
+    return render_template('index.html', items=items)
