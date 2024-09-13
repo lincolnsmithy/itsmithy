09597ccd154b6716a4e3b1632459c7b7ca5e3ec3
lincolnsmithy <lincolnsmithy@gmail.com>
2024-09-13 17:37:00 -0400

    more updated to text in base

--- itsmithy.py
+++ itsmithy.py
@@ -15,4 +15,4 @@ def hello_world():
     return render_template('index.html', items=items)
 
 if __name__ == '__main__':
-    app.run(debug=False,port=5000,host="0.0.0.0")
+    app.run(debug=False,port=8080,host="0.0.0.0")
